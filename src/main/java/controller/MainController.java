5573f2a99b8aa7043b46ed23b088a734822e7122
Hanna <hanna.maliszewska41@gmail.com>
2026-03-27 19:34:47 +0100

    Implement minimal GUI logic

--- src/main/java/controller/MainController.java
+++ src/main/java/controller/MainController.java
@@ -1,19 +1,72 @@
 package controller;
 
 import javafx.fxml.FXML;
-import javafx.scene.control.TextArea;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 
 public class MainController {
+
+    @FXML
+    private RadioButton textRadio;
+
+    @FXML
+    private RadioButton fileRadio;
+
+    @FXML
+    private Button readFileButton1;
+
+    @FXML
+    private Button writeFileButton1;
 
     @FXML
     private TextArea plainText;
 
     @FXML
-    private TextArea encryptedText;
+    private TextField keyField;
 
     @FXML
-    private TextField keyField;
+    private Button generateKeyButton;
+
+    @FXML
+    private Button loadKeyButton;
+
+    @FXML
+    private Button saveKeyButton;
+
+    @FXML
+    private Button readFileButton2;
+
+    @FXML
+    private Button writeFileButton2;
+
+    @FXML
+    private TextArea encryptedText;
+
+
+    @FXML
+    public void initialize() {
+        // Listener zmiany wyboru Text / File
+        ToggleGroup textOrFileGroup = textRadio.getToggleGroup();
+        textOrFileGroup.selectedToggleProperty().addListener((obs, oldToggle, newToggle) -> {
+            if (newToggle == textRadio) {
+                // Text wybrane
+                plainText.setEditable(true);
+                readFileButton1.setDisable(true);
+                writeFileButton1.setDisable(true);
+                readFileButton2.setDisable(true);
+                writeFileButton2.setDisable(true);
+            } else if (newToggle == fileRadio) {
+                // File wybrane
+                plainText.setEditable(false);
+                readFileButton1.setDisable(false);
+                writeFileButton1.setDisable(false);
+                readFileButton2.setDisable(false);
+                writeFileButton2.setDisable(false);
+            }
+        });
+
+        // Blokada pola klucza do wpisywania ręcznego
+        keyField.setEditable(false);
+    }
 
     @FXML
     public void generate() {
