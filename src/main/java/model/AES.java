daa14fdbbcf327c1672c993dc723e665c1fe18b8
Hanna <hanna.maliszewska41@gmail.com>
2026-03-28 20:47:53 +0100

    Implement shiftRows method + test

--- src/main/java/model/AES.java
+++ src/main/java/model/AES.java
@@ -35,4 +35,30 @@ public class AES {
 
         return state;
     }
+
+    public static byte[][] shiftRows(byte[][] state) {
+        byte[][] helper = new byte[4][4];
+
+        // first row doesn't change
+
+        // second row rotates by one byte
+        helper[1][0] = state[1][1];
+        helper[1][1] = state[1][2];
+        helper[1][2] = state[1][3];
+        helper[1][3] = state[1][0];
+
+        // third row rotates by two bytea
+        helper[2][0] = state[2][2];
+        helper[2][1] = state[2][3];
+        helper[2][2] = state[2][0];
+        helper[2][3] = state[2][1];
+
+        // fourth row rotates by three bytea
+        helper[3][0] = state[3][3];
+        helper[3][1] = state[3][0];
+        helper[3][2] = state[3][1];
+        helper[3][3] = state[3][2];
+
+        return helper;
+    }
 }
--- src/test/java/model/AESTest.java
+++ src/test/java/model/AESTest.java
@@ -14,4 +14,26 @@ public class AESTest {
 
         System.out.println(Integer.toHexString(state[0][0] & 0xFF));
     }
+
+    @Test
+    void testShiftRows() {
+        byte[][] state = new byte[4][4];
+        byte[][] test = {
+                {0, 0, 0, 0},
+                {11, 12, 13, 10},
+                {22, 23, 20, 21},
+                {33, 30, 31, 32},
+        };
+
+        for (int row = 0; row < 4; row++) {
+            for (int col = 0; col < 4; col++) {
+
+                state[row][col] = (byte) (10 * row + col);
+            }
+        }
+
+        state = AES.shiftRows(state);
+
+        assertArrayEquals(state, test);
+    }
 }
